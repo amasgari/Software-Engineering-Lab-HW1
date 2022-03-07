63e37fb1bb6c88300c75cd62f3bd7595f6876854
amasgari <amasgari@ce.sharif.edu>
2022-03-07 23:02:17 +0330

    Add initial logic of parsing

--- Parser.py
+++ Parser.py
@@ -1,11 +1,18 @@
 import re
+from ErrorMessages import *
 
 class Parser:
     def __init__(self):
         pass
 
     def parse(self, str_inp):
-        if re.search("[0-9]+[+\-*/][0-9]+", str_inp) is not None:
-            pass
+        if re.search("[0-9]+[+*/-][0-9]+", str_inp):
+            operand1, operand2 = [int(x) for x in re.split("[+*/-]", str_inp)]
+            operator = re.findall("[+*/-]", str_inp)[0]
+            return operand1, operand2, operator
+        elif re.search("[0-9]+[^+*/-][0-9]+", str_inp):
+            return ErrorMessages.UnOperator
+        elif re.search("[^0-9]*[+*/-][0-9]+") or re.search("[0-9]+[+*/-][^0-9]*"):
+            return ErrorMessages.InOperand
         else:
-            print("Invalid format of input")
+            return
