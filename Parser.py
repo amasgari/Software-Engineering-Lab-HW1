5416f4a65617614ffa600eb8aaf31e68d3c76cee
amasgari <amasgari@ce.sharif.edu>
2022-03-07 22:21:16 +0330

    Add valid format of input string for Parser

--- Parser.py
+++ Parser.py
@@ -1,6 +1,11 @@
+import re
+
 class Parser:
     def __init__(self):
         pass
 
     def parse(self, str_inp):
-        pass
+        if re.search("[0-9]+[+\-*/][0-9]+", str_inp) is not None:
+            pass
+        else:
+            print("Invalid format of input")
