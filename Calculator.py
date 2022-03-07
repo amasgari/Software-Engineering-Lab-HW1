f42e8a55ae57b85fcaec79748aec7f01c7962827
amasgari <amasgari@ce.sharif.edu>
2022-03-07 22:15:38 +0330

    Initial implementation of four basic operations

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,18 @@
 class Calculator:
     def __init__(self):
         pass
+
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        if b != 0:
+            return a / b
+        else:
+            return 'Divison by zero'
