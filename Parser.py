0426314d6e9d72d4fb8f5b472b4fabc3bba689ac
z <z.y.j.1379@gmail.com>
2022-03-09 00:12:57 +0330

    Check input validation in parser and returning related Error Messages

--- Parser.py
+++ Parser.py
@@ -1,18 +1,32 @@
 import re
 from ErrorMessages import *
 
+
 class Parser:
     def __init__(self):
         pass
 
+    def isNumber(self, n):
+        try:
+            a = float(n)
+            return True
+        except:
+            return False
+
     def parse(self, str_inp):
-        if re.search("[0-9]+[+*/-][0-9]+", str_inp):
-            operand1, operand2 = [int(x) for x in re.split("[+*/-]", str_inp)]
-            operator = re.findall("[+*/-]", str_inp)[0]
-            return operand1, operand2, operator
-        elif re.search("[0-9]+[^+*/-][0-9]+", str_inp):
-            return ErrorMessages.UnOperator
-        elif re.search("[^0-9]*[+*/-][0-9]+") or re.search("[0-9]+[+*/-][^0-9]*"):
-            return ErrorMessages.InOperand
+        t_inputs = str_inp.split(' ')
+        inputs = []
+        for inp in t_inputs:
+            inputs.append(inp.strip())
+        if len(inputs) < 3:
+            return ErrorMessages.UnknownFormat
         else:
-            return
+            if self.isNumber(inputs[0]) & self.isNumber(inputs[2]):
+                operand1, operand2 = float(inputs[0]), float(inputs[2])
+                operator = inputs[1]
+                if operator in ['+', '-', '/', '*']:
+                    return operand1, operand2, operator
+                else:
+                    return ErrorMessages.UnOperator
+            else:
+                return ErrorMessages.InOperand
