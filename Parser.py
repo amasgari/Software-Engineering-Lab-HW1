fcfe2de7f758cfd30bac278dfb4284f9bb757f1d
amasgari <amasgari@ce.sharif.edu>
2022-03-10 22:22:25 +0330

    Support "^" as symbol of power and add unit test for power operation

--- Parser.py
+++ Parser.py
@@ -32,7 +32,7 @@ class Parser:
             operator = inputs[1]
             if not operand1 or not operand2:
                 return ErrorMessages.InvalidOperand
-            if operator not in ['+', '-', '/', '*']:
+            if operator not in ['+', '-', '/', '*', '^']:
                 return ErrorMessages.UnsupportedOperator
             return operand1, operand2, operator
 
@@ -43,6 +43,7 @@ class TestParser(unittest.TestCase):
         self.assertEqual(Parser.parse('3.2e2 * 5.0'), (320.0, 5.0, Operator.Multiply.value))
         self.assertEqual(Parser.parse('1.1 - 6E-0'), (1.1, 6.0, Operator.Minus.value))
         self.assertEqual(Parser.parse('8.4 / 11'), (8.4, 11.0, Operator.Division.value))
+        self.assertEqual(Parser.parse('3e1 ^ 2'), (30.0, 2.0, Operator.Power.value))
 
     def test_invalid_operand_err(self):
         self.assertEqual(Parser.parse('2f1 + -4e0'), ErrorMessages.InvalidOperand)
